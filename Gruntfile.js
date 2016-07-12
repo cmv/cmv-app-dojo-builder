73cd639b01fd0e25f9807b87914b379315fc8c5e
roemhildtg <roemhildtg@gmail.com>
2016-07-12 17:56:02 -0500

    Strip debug flag on build.
    Partially fixes https://github.com/roemhildtg/cmv-app-dojo-builder/issues/1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,99 +1,114 @@
 var fs = require('fs'),
-  path = require('path');
+    path = require('path');
 
 function isEmptyDirectory(filepath) {
-  var children, x;
-  if (!fs.statSync(filepath).isDirectory()) {
-    return false;
-  }
-  children = fs.readdirSync(filepath);
-  for (x in children) {
-    if (!isEmptyDirectory(path.join(filepath, children[x]))) {
-      return false;
+    var children, x;
+    if (!fs.statSync(filepath).isDirectory()) {
+        return false;
     }
-  }
-  return true;
+    children = fs.readdirSync(filepath);
+    for (x in children) {
+        if (!isEmptyDirectory(path.join(filepath, children[x]))) {
+            return false;
+        }
+    }
+    return true;
 }
 
-module.exports = function (grunt) {
-  // Build customizations would be left up to developer to implement.
-  grunt.loadNpmTasks('grunt-dojo');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.initConfig({
-    clean: {
-      build: {
-        src: ['dist/']
-      },
-      uncompressed: {
-        src: [
-          'dist/**/*.uncompressed.js'
-        ]
-      },
-      afterbuild: {
-        src: [
+module.exports = function(grunt) {
+    // Build customizations would be left up to developer to implement.
+    grunt.loadNpmTasks('grunt-dojo');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-strip-code');
+    grunt.initConfig({
+        strip_code: {
+            options: {
+                blocks: [{
+                    start_block: '<!--debug-start-->',
+                    end_block: '<!--debug-end-->'
+                }, {
+                    start_block: '/* debug-start */',
+                    end_block: '/* debug-end */'
+                }]
+            },
+            your_target: {
+                src: 'dist/index.html'
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            },
+            uncompressed: {
+                src: [
+                    'dist/**/*.uncompressed.js'
+                ]
+            },
+            afterbuild: {
+                src: [
 
-          //lets delete everything!
-          'dist/**/*',
+                    //lets delete everything!
+                    'dist/**/*',
 
-          //...except a few things
-          '!dist/index.html',
+                    //...except a few things
+                    '!dist/index.html',
 
-          //dojo built layers and css files
-          //include other layers, or they'll be deleted
-          '!dist/dojo/dojo.js',
-          '!dist/app/css/build.css',
+                    //dojo built layers and css files
+                    //include other layers, or they'll be deleted
+                    '!dist/dojo/dojo.js',
+                    '!dist/app/css/build.css',
 
 
-          //the build output in case something goes wrong
-          '!dist/build-report.txt',
+                    //the build output in case something goes wrong
+                    '!dist/build-report.txt',
 
-          //any additional configs
-          //'!dist/**/app/config/**/*.js',
+                    //any additional configs
+                    //'!dist/**/app/config/**/*.js',
 
-          //images, resources, and source maps
-          '!dist/**/fonts/**',
-          '!dist/**/images/**',
-          '!dist/**/resources/**',
-          '!dist/**/*.js.map'
-        ],
-        filter: 'isFile'
-      },
-      emptydir: {
-        src: ['dist/**/*'],
-        filter: isEmptyDirectory
-      }
-    },
-    copy: {
-      main: {
-        files: [{
-          expand: true,
-          cwd: 'src/',
-          src: ['index.html'],
-          dest: './dist/',
-          rename: function(dest, src) {
-            return dest + 'index.html';
-          }
-        }]
-      }
-    },
-    dojo: {
-      dist: {
-        options: {
-          releaseDir: '../dist',
+                    //images, resources, and source maps
+                    '!dist/**/fonts/**',
+                    '!dist/**/images/**',
+                    '!dist/**/resources/**',
+                    '!dist/**/*.js.map'
+                ],
+                filter: 'isFile'
+            },
+            emptydir: {
+                src: ['dist/**/*'],
+                filter: isEmptyDirectory
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['index.html'],
+                    dest: './dist/',
+                    rename: function(dest, src) {
+                        return dest + 'index.html';
+                    }
+                }]
+            }
+        },
+        dojo: {
+            dist: {
+                options: {
+                    releaseDir: '../dist',
+                }
+            },
+            options: {
+                profile: 'build.profile.js',
+                dojo: 'src/dojo/dojo.js',
+                load: 'build',
+                cwd: './',
+                basePath: './src'
+            }
         }
-      },
-      options: {
-        profile: 'build.profile.js',
-        dojo: 'src/dojo/dojo.js',
-        load: 'build',
-        cwd: './',
-        basePath: './src'
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('build-dev', ['clean:build', 'dojo', 'copy']);
-  grunt.registerTask('build', ['clean:build', 'dojo', 'copy', 'clean:afterbuild', 'clean:emptydir']);
+    grunt.registerTask('build-dev', ['clean:build', 'dojo', 'copy']);
+    grunt.registerTask('build', ['clean:build', 'dojo', 'copy', 'clean:afterbuild', 'clean:emptydir', 'strip_code']);
 
 };

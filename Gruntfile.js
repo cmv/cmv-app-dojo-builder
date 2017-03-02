a92550f6fdf0db21dc1145ac569a642423e4183f
Gregg Roemhildt <roemhildtg@gmail.com>
2017-03-02 17:39:11 -0600

    strip debug flag on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
                 }]
             },
             your_target: {
-                src: 'dist/index.html'
+                src: 'dist/dojoConfig.js'
             }
         },
         clean: {
